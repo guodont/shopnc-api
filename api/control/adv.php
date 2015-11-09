9e0a4b4ffefa9f8a3ded93f3b19f18ed63cd3a4f
guodont <guodont@163.com>
2015-11-09 18:19:12 +0800

    fix app

--- api/control/adv.php
+++ api/control/adv.php
@@ -5,6 +5,7 @@
  * Date: 15/11/9
  * Time: 下午3:52
  */
+defined('InShopNC') or exit('Access Invalid!');
 
 class advControl extends apiControl
 {
@@ -13,22 +14,42 @@ class advControl extends apiControl
         parent::__construct();
     }
 
+    // utf-8
+    private function mb_unserialize($serial_str) {
+        $serial_str= preg_replace_callback('!s:(\d+):"(.*?)";!s', function($r) {
+            return 's:'.strlen($r[2]).':"'.$r[2].'";';
+        }, $serial_str );
+        $serial_str= str_replace("\r", "", $serial_str);
+        return unserialize($serial_str);
+    }
+
+    // ascii
+    private function asc_unserialize($serial_str) {
+        $serial_str = preg_replace('!s:(\d+):"(.*?)";!se', '"s:".strlen("$2").":\"$2\";"', $serial_str );
+        $serial_str= str_replace("\r", "", $serial_str);
+        return unserialize($serial_str);
+    }
+
     /**
      * GET 获取app首页广告
      */
     public function getAppHomeAdvOp()
     {
-        $m_adv = new Model('new_adv');
-        $advs = array();
+        $m_adv = Model('new_adv');
         $where = array();
         $where['ap_id'] = 1048;
-        $advs = $m_adv ->getList($where);
-        foreach($advs as $key => $val )
-        {
-            $pic_content[$key] = unserialize($val['adv_content']);
-            $advs[$key]['img_url'] = $pic_content[$key]['adv_pic'];
-            $advs[$key]['adv_url'] = $pic_content[$key]['adv_pic_url'];
+        $advs = $m_adv->getList($where, '', '*');
+        foreach ($advs as $key => $val) {
+            $val = $advs[$key]['adv_content'];
+            $pic_content[$key] = $this->mb_unserialize($val);
+            $pic = $pic_content[$key]['adv_pic'];
+            $url = $pic_content[$key]['adv_pic_url'];
+            $advs[$key]['img_url'] = $pic;
+            $advs[$key]['adv_url'] = $url;
         }
         output_data($advs);
     }
+
+
+
 }
--- data/model/new_adv.model.php
+++ data/model/new_adv.model.php
@@ -6,38 +6,17 @@
  * Date: 15/11/9
  * Time: 下午3:30
  */
-class new_adv extends Model
+defined('InShopNC') or exit('Access Invalid!');
+
+class new_advModel extends Model
 {
-
-    /**
-     * 缓存数据
-     */
-    protected $cachedData;
-
     public function __construct()
     {
         parent::__construct('adv');
     }
 
-    public function getCache() {
-        if ($this->cachedData) {
-            return $this->cachedData;
-        }
-        $data = rkcache('adv');
-        if (!$data) {
-            $data = array();
-            foreach ((array) $this->getList(array()) as $v) {
-                $id = $v['adv_id'];
-                $data['data'][$id] = $v;
-            }
-            wkcache('adv', $data);
-        }
-        return $this->cachedData = $data;
-    }
-
     public function getList($condition=array(), $page='', $field, $limit='', $order='slide_sort, adv_id desc', $count = 0){
         return $this->table('adv')->field($field)->where($condition)->order($order)->limit($limit)->page($page, $count)->select();
     }
-
 
 }
