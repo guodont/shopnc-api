6eb70af5a1245ac3a85495cb40737720163bc6eb
guodont <guodont@163.com>
2015-11-13 11:39:22 +0800

    服务api

---
+++ api/control/service.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * 服务分类 api
+ */
+defined('InShopNC') or exit('Access Invalid!');
+
+class service_classControl extends apiHomeControl
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * GET 服务列表
+     */
+    public function servicesOp()
+    {
+
+        $model_service = Model('service');
+
+        //  排序
+        $condition['keyword'] = trim($_GET['search_service_name']);
+        $condition['brand_id'] = intval($_GET['search_brand_id']);
+        $condition['gc_id'] = intval($_GET['cate_id']);
+
+        $service_list = $model_service->geServiceList($condition,$this->page);
+
+        output_data(array('services' => $service_list));
+
+    }
+
+    /**
+     * GET 服务详情
+     */
+    public function serviceOp()
+    {
+        if (!isset($_GET['sid'])) {
+            output_error("缺少服务id参数");
+            die;
+        }
+        $service_id = $_GET['sid'];
+        $m_service = Model('serviceapi');
+
+        $where = array('service_id' => $service_id);
+        $service_info = $m_service->where($where)->order('service_id desc')->select();
+
+        // TODO 服务收藏
+//        if (intval($_GET['fav_id']) > 0) {
+//            $favorites_class = Model('flea_favorites');
+//            if (!$favorites_class->checkFavorites(intval($_GET['fav_id']), 'flea', intval($_GET['user_id']))) {
+//                $service_info[0][is_favorite] = false;
+//            }
+//        }
+
+//        $service_info[0]['member_avatar'] = getMemberAvatarForID($service_info['member_id']);
+
+//        $goods_image_path = UPLOAD_SITE_URL . DS . ATTACH_MALBUM . '/' . $service_info[0]['member_id'] . '/';;    //店铺商品图片目录地址
+//
+//        $desc_image = $m_service->getListImageService(array('image_store_id' => $service_info[0]['member_id'], 'item_id' => $service_info[0]['goods_id'], 'image_type' => 12));
+//
+//        $m_service->getThumb($desc_image, $goods_image_path);
+
+        //        $image_key = 0;
+//
+//        if (!empty($desc_image) && is_array($desc_image)) {//将封面图放到第一位显示
+//            $goods_image_1 = $service_info[0]['goods_image'];//封面图
+//            foreach ($desc_image as $key => $val) {
+//                if ($goods_image_1 == $val['thumb_small']) {
+//                    $image_key = $key;
+//                    break;
+//                }
+//            }
+//            if ($image_key > 0) {//将封面图放到第一位显示
+//                $desc_image_0 = $desc_image[0];
+//                $desc_image[0] = $desc_image[$image_key];
+//                $desc_image[$image_key] = $desc_image_0;
+//            }
+//        }
+//
+//        $service_info[0]['goods_image'] = $desc_image;
+
+        /**
+         * 浏览次数更新
+         */
+        $m_service->updateService(array('service_click' => ($service_info[0]['service_click'] + 1)), $service_id);
+
+        if (!empty($service_info)) {
+            output_data(array('service_info' => $service_info));
+        } else {
+            output_error("没有此交易");
+        }
+    }
+}
---
+++ data/model/serviceapi.model.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * 服务api model
+ */
+defined('InShopNC') or exit('Access Invalid!');
+
+class serviceapiModel extends Model
+{
+
+    public function __construct()
+    {
+        parent::__construct('service');
+    }
+
+
+    /**
+     *
+     * 获取服务列表
+     * @param $condition
+     * @param string $field
+     * @param string $order
+     * @param int $limit
+     * @param int $page
+     * @return mixed
+     */
+    public function geServiceList($condition, $field = '*', $order = '', $limit = 0, $page = 0)
+    {
+        return $this->field($field)->where($condition)->order($order)->limit($limit)->page($page)->select();
+    }
+
+    /**
+     * 服务详情多图
+     *
+     * @param	array $param 列表条件
+     * @param	array $field 显示字段
+     */
+    public function getListImageService($param,$field='*') {
+        if(empty($param)) {
+            return false;
+        }
+        //得到条件语句
+        $condition_str	= $this->getCondition($param);
+        $array	= array();
+        $array['table']		= 'flea_upload';
+        $array['where']		= $condition_str;
+        $array['field']		= $field;
+        $list_image			= Db::select($array);
+
+        return $list_image;
+    }
+
+    /**
+     * 得到所有缩略图，带路径
+     *
+     * @param	array $goods 商品列表
+     */
+    public function getThumb(&$goods,$path){
+        if (is_array($goods)){
+            foreach ($goods as $k=>$v) {
+                $goods[$k]['thumb_small'] 	= $path.$v['file_thumb'];
+                $goods[$k]['thumb_mid'] 	= $path.str_replace('_small','_mid',$v['file_thumb']);
+                $goods[$k]['thumb_max'] 	= $path.str_replace('_small','_max',$v['file_thumb']);
+            }
+        }
+    }
+
+    /**
+     * 服务信息更新
+     *
+     * @param	array $param 列表条件
+     * @param	int $service_id 服务id
+     */
+    public function updateService($param,$service_id) {
+
+        if(empty($param)) {
+            return false;
+        }
+
+        $update		= false;
+
+        if(is_array($service_id))$service_id	= implode(',',$service_id);
+        //得到条件语句
+        $condition_str	= "WHERE service_id in(".$service_id.")";
+
+        $update = Db::update('service',$param,$condition_str);
+
+        return $update;
+    }
+}
