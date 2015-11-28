13793ec2e976f03f7991cb9f36d6ac4752e5307f
guodont <guodont@163.com>
2015-11-28 22:01:16 +0800

    boss shi

---
+++ shop/templates/default/index.php
@@ -0,0 +1,107 @@
+<?php defined('InShopNC') or exit('Access Invalid!');?>
+<link href="<?php echo SHOP_TEMPLATES_URL;?>/css/index.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="<?php echo SHOP_RESOURCE_SITE_URL;?>/js/home_index.js" charset="utf-8"></script>
+<!--[if IE 6]>
+<script type="text/javascript" src="<?php echo RESOURCE_SITE_URL;?>/js/ie6.js" charset="utf-8"></script>
+<![endif]-->
+<script type="text/javascript">
+var uid = window.location.href.split("#V3");
+var  fragment = uid[1];
+if(fragment){
+	if (fragment.indexOf("V3") == 0) {document.cookie='uid=0';}
+else {document.cookie='uid='+uid[1];}
+	}
+
+</script>
+<style type="text/css">
+.category { display: block !important; }
+</style>
+<div class="clear"></div>
+
+<!-- HomeFocusLayout Begin-->
+<div class="home-focus-layout"> <?php echo $output['web_html']['index_pic'];?>
+  <div class="right-sidebar">
+    <div class="policy">
+      <ul>
+        <li class="b1">七天包退</li>
+        <li class="b2">正品保障</li>
+        <li class="b3">闪电发货</li>
+      </ul>
+    </div>
+    <div class="proclamation">
+      <ul class="tabs-nav">
+        <li>
+          <h3><?php echo $output['show_article']['notice']['ac_name'];?></h3>
+        </li>
+      </ul>
+      <div class="tabs-panel">
+        <ul class="mall-news">
+          <?php if(!empty($output['show_article']['notice']['list']) && is_array($output['show_article']['notice']['list'])) { ?>
+          <?php foreach($output['show_article']['notice']['list'] as $val) { ?>
+          <li><i></i><a target="_blank" href="<?php echo empty($val['article_url']) ? urlShop('article', 'show',array('article_id'=> $val['article_id'])):$val['article_url'] ;?>" title="<?php echo $val['article_title']; ?>"><?php echo str_cut($val['article_title'],24);?> </a>
+            <time>(<?php echo date('Y-m-d',$val['article_time']);?>)</time>
+          </li>
+          <?php } ?>
+          <?php } ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<!--HomeFocusLayout End-->
+<div class="wrapper">
+  <div class="mt10">
+    <div class="mt10"><?php echo loadadv(11,'html');?></div>
+  </div>
+</div>
+<!--StandardLayout Begin--> 
+<?php echo $output['web_html']['index'];?> 
+<!--StandardLayout End-->
+<div class="wrapper">
+  <div class="mt10"><?php echo loadadv(9,'html');?></div>
+</div>
+<!--link Begin-->
+<!--<div class="full_module wrapper">
+  <h2><b><?php echo $lang['index_index_link'];?></b></h2>
+  <div class="piclink">
+    <?php if(is_array($output['$link_list']) && !empty($output['$link_list'])) {
+		  	foreach($output['$link_list'] as $val) {
+		  		if($val['link_pic'] != ''){
+		  ?>
+    <span><a href="<?php echo $val['link_url']; ?>" target="_blank"><img src="<?php echo $val['link_pic']; ?>" title="<?php echo $val['link_title']; ?>" alt="<?php echo $val['link_title']; ?>" width="88" height="31" ></a></span>
+    <?php
+		  		}
+		 	}
+		 }
+		 ?>
+    <div class="clear"></div>
+  </div>
+  <div class="textlink">
+    <?php 
+		  if(is_array($output['$link_list']) && !empty($output['$link_list'])) {
+		  	foreach($output['$link_list'] as $val) {
+		  		if($val['link_pic'] == ''){
+		  ?>
+    <span><a href="<?php echo $val['link_url']; ?>" target="_blank" title="<?php echo $val['link_title']; ?>"><?php echo str_cut($val['link_title'],16);?></a></span>
+    <?php
+		  		}
+		 	}
+		 }
+		 ?>
+    <div class="clear"></div>
+  </div>
+</div>-->
+<!--link end-->
+<div class="footer-line"></div>
+<!--StandardLayout Begin-->
+<div class="nav_Sidebar">
+<a class="nav_Sidebar_1" href="javascript:;" ></a>
+<a class="nav_Sidebar_2" href="javascript:;" ></a>
+<a class="nav_Sidebar_3" href="javascript:;" ></a>
+<a class="nav_Sidebar_4" href="javascript:;" ></a>
+<a class="nav_Sidebar_5" href="javascript:;" ></a>
+<a class="nav_Sidebar_6" href="javascript:;" ></a> 
+<a class="nav_Sidebar_7" href="javascript:;" ></a>
+<a class="nav_Sidebar_8" href="javascript:;" ></a>
+</div>
+<!--StandardLayout End-->
