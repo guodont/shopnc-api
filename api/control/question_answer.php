3647e838b9d12b9f3176212c053f04c6b4c3d0db
guodont <guodont@163.com>
2015-12-04 22:25:52 +0800

    fix

--- api/control/question_answer.php
+++ api/control/question_answer.php
@@ -174,45 +174,31 @@ class question_answerControl extends apiMemberControl
     public function adoptAnswerOp()
     {
         //  获取问题id 回答id
-
         $question_id = trim($_POST['qid']);
-
         $reply_id = trim($_POST['rid']);
-
         if (empty($question_id) || empty($reply_id)) {
-            echo 0;
-            die;
+            echo 0;die;
         }
-
         $model = Model();
-
         //  获取问题数据
         $question = $model->table('circle_theme')->where(array('theme_id' => $question_id))->find();
-
         //  验证是否本人提问
         if ($question['member_id'] != $this->member_info['member_id']) {
-            echo 0;
-            die;
+            echo 0;die;
         }
-
         //  问题状态为未完成
         if ($question['theme_state'] == 1) {
-            echo 0;
-            die;
+            echo 0;die;
         }
-
         //  获取回复数据
         $answer = $model->table('circle_threply')->where(array('reply_id' => $reply_id))->find();
 
         //  验证非本人回答的
         if ($answer['member_id'] == $this->member_info['member_id']) {
-            echo 0;
-            die;
+            echo 0;die;
         }
-
         //  获取悬赏金币
         $reward_count = trim($question['theme_reward']);
-
         //  给被采纳用户金币并记录日志
         $insert_arr = array();
         $insert_arr['pl_memberid'] = $answer['member_id'];
@@ -227,9 +213,7 @@ class question_answerControl extends apiMemberControl
         $update_question = $model->table('circle_theme')->where(array('theme_id' => $question_id))->update($update2);
         $update_answer = $model->table('circle_threply')->where(array('reply_id' => $reply_id))->update($update1);
         if ($update_answer && $update_question) {
-            //  echo 1
-            echo 1;
-            die;
+            echo 1;die;
         }
     }
 
